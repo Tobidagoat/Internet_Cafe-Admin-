eb614e81720b0646a6c0f4e8d7b35fa147272b83
Tobi <ryoko12fu@gmail.com>
2025-06-12 15:50:30 +0630

    updated room code

--- src/controller/RoomController.java
+++ src/controller/RoomController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import model.room;
 
@@ -35,7 +37,7 @@ public class RoomController implements Initializable {
         
         
         for(room room:roomlist){
-            //AnchorPane roomcard=createroomcard(room);
+            AnchorPane roomcard=createroomcard(room);
         }
     }    
 
@@ -47,11 +49,30 @@ public class RoomController implements Initializable {
     private void btnprivateaction(ActionEvent event) {
     }
 
-//    private AnchorPane createroomcard(room room) {
-//        AnchorPane card=new AnchorPane();
-//        card.setPrefSize(274, 274);
-//        
-//        
-//    }
+    private AnchorPane createroomcard(room room) {
+        AnchorPane card=new AnchorPane();
+        card.setPrefSize(274, 274);
+        
+        Label namelabel=new Label(room.getName());
+        namelabel.setStyle("-fx-font-size:22px;"
+                + "-fx-font-weight: bold;");
+        
+        
+        AnchorPane.setTopAnchor(namelabel, 35.0);
+        AnchorPane.setLeftAnchor(namelabel, 10.0);
+        AnchorPane.setRightAnchor(namelabel, 10.0);
+        
+        namelabel.setMaxWidth(Double.MAX_VALUE);
+        namelabel.setAlignment(Pos.CENTER);
+        
+        card.setStyle("-fx-background-color: #202225;"
+                + "-fx-background-radius: 4px;"
+                + "-fx-border-radius: 4px;"
+                + "-fx-border-color: #494949;");
+        
+        card.getChildren().add(namelabel);
+        
+        return card;
+    }
     
 }
